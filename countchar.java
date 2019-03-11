3557d539add097991f7e1dfb1e721a64708614cf
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-03-11 13:07:59 -0700

    Create countchar.java

---
+++ countchar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+int count;
+for (int i=0;i<s.length;i++)
+{
+temp=s.charAt[i];
+if(temp.isLetter)
+count++;
+}
+Systemout.println(count);
+}}
+
+
