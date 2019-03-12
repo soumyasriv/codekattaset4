abb6de733d66c8264e758412d4326c185204ffbc
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-03-11 20:10:17 -0700

    Create charcount.java

---
+++ charcount.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class main
+{
+public static void main (String[] args)
+{
+Scanner sc=new Scanner(System.in)
+String str=sc.nextLine();
+char[] ch=s.toCharArray();
+int i=0,count=0;
+String s="";
+while(i<=ch.length-1)
+{
+if (ch(i)=='')
+{i++;}
+s+=ch[i];
+count++;
+i++;
+
+}
+System.out.println(count);
+
+
+
+}}
