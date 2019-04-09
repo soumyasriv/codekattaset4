b764259c4013677e217a3b7d5d2a5de8e0d71d1a
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-04-08 19:52:54 -0700

    Create duplicate.java

---
+++ duplicate.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Solution
+{
+public staic void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt();
+int n=sc.nextInt();
+int temp;
+temp=m;
+m=n;
+n=temp;
+System.out.print(m+" ");
+System.ougt.print(n);
+}}
